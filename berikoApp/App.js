5c3eef71a5cea559fa4d0a463ef92f9a9a086f8e
Raghavan M <20pw26@psgtech.ac.in>
2023-03-09 14:13:39 +0530

    working on landing page

--- berikoApp/App.js
+++ berikoApp/App.js
@@ -1,20 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {View, Text, Image, ScrollView, TextInput,StyleSheet} from 'react-native';
+
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+  return(
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+      
+      <View style={{flex:1,justifyContent:'center',alignItems:'center',backgroundColor:'#243236'}}>
+        <Text style={styles.text}>Beriko</Text>
+        
+      </View>
+     
+    
+)};
+const styles=StyleSheet.create({
+ text:{
+  color:'white',
+  fontSize:37,
+  fontFamily: 'Nunito',
+ },
+
 });
+
