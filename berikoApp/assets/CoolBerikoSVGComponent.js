59b0302bcfb7c710237c5a01585989410f1a1737
Raghavan M <20pw26@psgtech.ac.in>
2023-03-17 02:30:48 +0530

    did basic routing but isnt good

---
+++ berikoApp/assets/CoolBerikoSVGComponent.js
@@ -0,0 +1,162 @@
+import * as React from "react";
+import Svg, { Defs, G, Rect, Path } from "react-native-svg";
+/* SVGR has dropped some elements not supported by react-native-svg: desc */
+const CoolBerikoSVGComponent = (props) => (
+  <Svg
+    xmlns="http://www.w3.org/2000/svg"
+    xmlnsXlink="http://www.w3.org/1999/xlink"
+    width={1080}
+    height={1080}
+    viewBox="0 0 1080 1080"
+    xmlSpace="preserve"
+    {...props}
+  >
+    <Defs />
+    <G
+      transform="matrix(1 0 0 1 540 540)"
+      id="235da4b2-b0d8-45a0-9025-8c550d25fcf7"
+    >
+      <Rect
+        style={{
+          stroke: "none",
+          strokeWidth: 1,
+          strokeDasharray: "none",
+          strokeLinecap: "butt",
+          strokeDashoffset: 0,
+          strokeLinejoin: "miter",
+          strokeMiterlimit: 4,
+          fill: "rgb(255,255,255)",
+          fillRule: "nonzero",
+          opacity: 1,
+          visibility: "hidden",
+        }}
+        vectorEffect="non-scaling-stroke"
+        x={-540}
+        y={-540}
+        rx={0}
+        ry={0}
+        width={1080}
+        height={1080}
+      />
+    </G>
+    <G
+      transform="matrix(1 0 0 1 540 540)"
+      id="b9c8488b-0ebf-4032-8a18-463b4e736bb9"
+    />
+    <G transform="matrix(1 0 0 1 565.04 489.92)">
+      <G style={{}} vectorEffect="non-scaling-stroke">
+        <G transform="matrix(6.25 0 0 6.25 0 28.41)">
+          <Path
+            style={{
+              stroke: "none",
+              strokeWidth: 1,
+              strokeDasharray: "none",
+              strokeLinecap: "butt",
+              strokeDashoffset: 0,
+              strokeLinejoin: "miter",
+              strokeMiterlimit: 4,
+              fill: "rgb(8,204,126)",
+              fillRule: "nonzero",
+              opacity: 1,
+            }}
+            vectorEffect="non-scaling-stroke"
+            transform=" translate(-34.65, -36.56)"
+            d="M 47.6 27.8 C 43.800000000000004 21.6 44.1 11.100000000000001 34.2 11.100000000000001 C 24.300000000000004 11.100000000000001 25.300000000000004 21.6 21.6 27.8 C 18.900000000000002 32.3 13.400000000000002 38.4 13.400000000000002 45 C 13.400000000000002 67.7 55.900000000000006 67.7 55.900000000000006 45 C 55.800000000000004 38.4 50.300000000000004 32.3 47.60000000000001 27.8"
+            strokeLinecap="round"
+          />
+        </G>
+        <G transform="matrix(6.25 0 0 6.25 -1.67 -155.53)">
+          <Path
+            style={{
+              stroke: "none",
+              strokeWidth: 1,
+              strokeDasharray: "none",
+              strokeLinecap: "butt",
+              strokeDashoffset: 0,
+              strokeLinejoin: "miter",
+              strokeMiterlimit: 4,
+              fill: "rgb(148,113,81)",
+              fillRule: "nonzero",
+              opacity: 1,
+            }}
+            vectorEffect="non-scaling-stroke"
+            transform=" translate(-34.38, -7.13)"
+            d="M 35.2 2.1 C 35.1 1.8 34.1 2.3000000000000003 34.2 2.6 C 35.900000000000006 7.300000000000001 33.800000000000004 10.9 33.300000000000004 11.4 C 32.6 12.1 34.2 12.700000000000001 34.900000000000006 11.8 C 35.00000000000001 11.600000000000001 36.300000000000004 6.800000000000001 35.2 2.1000000000000014"
+            strokeLinecap="round"
+          />
+        </G>
+      </G>
+    </G>
+    <G transform="matrix(NaN NaN NaN NaN 0 0)">
+      <G style={{}} />
+    </G>
+    <G transform="matrix(NaN NaN NaN NaN 0 0)">
+      <G style={{}} />
+    </G>
+    <G transform="matrix(0.55 0 0 0.55 564.33 476.74)">
+      <G style={{}} vectorEffect="non-scaling-stroke">
+        <G transform="matrix(1 0 0 1 -86.66 0)">
+          <Path
+            style={{
+              stroke: "none",
+              strokeWidth: 1,
+              strokeDasharray: "none",
+              strokeLinecap: "butt",
+              strokeDashoffset: 0,
+              strokeLinejoin: "miter",
+              strokeMiterlimit: 4,
+              fill: "rgb(0,0,0)",
+              fillRule: "nonzero",
+              opacity: 1,
+            }}
+            vectorEffect="non-scaling-stroke"
+            transform=" translate(-61.84, -148.5)"
+            d="M 115.167 101.47 L 24.012 101.47 C 10.751 101.469 0 112.22 0 125.482 L 0 168.959 C 0 183.633 11.896 195.529 26.57 195.529 L 66.24000000000001 195.529 C 88.21700000000001 195.529 107.46300000000001 180.792 113.19400000000002 159.576 L 123.49100000000001 114.24 C 124.12400000000001 110.673 123.21500000000002 107.008 121.06000000000002 104.43599999999999 C 120.132 103.328 118.132 101.47 115.167 101.47 z M 24.542 138.106 C 22.597 138.106 20.654000000000003 137.364 19.169 135.881 C 16.202 132.913 16.202 128.102 19.169 125.134 L 24.597 119.706 C 27.565 116.74000000000001 32.376000000000005 116.74000000000001 35.344 119.706 C 38.311 122.674 38.311 127.485 35.344 130.453 L 29.916 135.881 C 28.432 137.364 26.487 138.106 24.542 138.106 z M 64.111 130.453 L 42.401 152.163 C 40.917 153.64600000000002 38.972 154.388 37.027 154.388 C 35.082 154.388 33.138 153.64600000000002 31.653000000000002 152.163 C 28.686000000000003 149.19500000000002 28.686000000000003 144.38400000000001 31.653000000000002 141.416 L 53.363 119.70599999999999 C 56.331 116.74 61.141999999999996 116.74 64.11 119.70599999999999 C 67.078 122.674 67.078 127.485 64.111 130.453 z"
+            strokeLinecap="round"
+          />
+        </G>
+        <G transform="matrix(1 0 0 1 -0.87 -30.48)">
+          <Path
+            style={{
+              stroke: "none",
+              strokeWidth: 1,
+              strokeDasharray: "none",
+              strokeLinecap: "butt",
+              strokeDashoffset: 0,
+              strokeLinejoin: "miter",
+              strokeMiterlimit: 4,
+              fill: "rgb(0,0,0)",
+              fillRule: "nonzero",
+              opacity: 1,
+            }}
+            vectorEffect="non-scaling-stroke"
+            transform=" translate(-147.63, -118.02)"
+            d="M 160.885 106.524 L 134.368 106.524 C 134.94299999999998 109.869 134.95499999999998 113.352 134.347 116.831 L 132.127 129.524 L 163.127 129.524 L 160.907 116.831 C 160.299 113.352 160.311 109.868 160.885 106.524 z"
+            strokeLinecap="round"
+          />
+        </G>
+        <G transform="matrix(1 0 0 1 84.91 0)">
+          <Path
+            style={{
+              stroke: "none",
+              strokeWidth: 1,
+              strokeDasharray: "none",
+              strokeLinecap: "butt",
+              strokeDashoffset: 0,
+              strokeLinejoin: "miter",
+              strokeMiterlimit: 4,
+              fill: "rgb(0,0,0)",
+              fillRule: "nonzero",
+              opacity: 1,
+            }}
+            vectorEffect="non-scaling-stroke"
+            transform=" translate(-233.41, -148.5)"
+            d="M 272.988 101.47 L 178.339 101.47 C 175.374 101.47 173.374 103.328 172.446 104.43599999999999 C 170.291 107.008 169.382 110.672 170.016 114.24 L 180.31199999999998 159.576 C 186.04299999999998 180.792 205.289 195.53 227.266 195.53 L 270.43 195.53 C 285.104 195.529 297 183.633 297 168.959 L 297 125.482 C 297 112.22 286.249 101.47 272.988 101.47 z M 192.422 135.881 C 189.45499999999998 132.913 189.45499999999998 128.102 192.422 125.134 L 197.85 119.706 C 200.81799999999998 116.74000000000001 205.629 116.74000000000001 208.59699999999998 119.706 C 211.564 122.674 211.564 127.485 208.59699999999998 130.453 L 203.16899999999998 135.881 C 201.68499999999997 137.364 199.73999999999998 138.106 197.795 138.106 C 195.851 138.106 193.906 137.364 192.422 135.881 z M 237.363 130.453 L 215.653 152.163 C 214.16899999999998 153.64600000000002 212.224 154.388 210.279 154.388 C 208.334 154.388 206.39 153.64600000000002 204.905 152.163 C 201.938 149.19500000000002 201.938 144.38400000000001 204.905 141.416 L 226.615 119.70599999999999 C 229.583 116.74 234.394 116.74 237.36200000000002 119.70599999999999 C 240.33 122.674 240.33 127.485 237.363 130.453 z"
+            strokeLinecap="round"
+          />
+        </G>
+      </G>
+    </G>
+  </Svg>
+);
+export default CoolBerikoSVGComponent;
---
+++ berikoApp/components/pages/SecondPage.js
@@ -0,0 +1,209 @@
+import { StatusBar } from 'expo-status-bar';
+import { Alert,View, Text, Image, ScrollView, TextInput,StyleSheet,TouchableHighlight,TouchableOpacity,} from 'react-native';
+import BerikoSVGComponent from '../../assets/BerikoSVGComponent';
+import ArrowSVGComponent from '../../assets/ArrowSVGComponent';
+import FontStyles from '../../assets/Fontstyle';
+import {NavigationContainer} from '@react-navigation/native';
+import CoolBerikoSVGComponent from '../../assets/CoolBerikoSVGComponent';
+import DullBerikoSVGComponent from '../../assets/DullBerikoSVGComponent';
+import React, { useState, useEffect } from 'react';
+
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import {
+  useFonts,
+  NotoSans_100Thin,
+  NotoSans_100Thin_Italic,
+  NotoSans_200ExtraLight,
+  NotoSans_200ExtraLight_Italic,
+  NotoSans_300Light,
+  NotoSans_300Light_Italic,
+  NotoSans_400Regular,
+  NotoSans_400Regular_Italic,
+  NotoSans_500Medium,
+  NotoSans_500Medium_Italic,
+  NotoSans_600SemiBold,
+  NotoSans_600SemiBold_Italic,
+  NotoSans_700Bold,
+  NotoSans_700Bold_Italic,
+  NotoSans_800ExtraBold,
+  NotoSans_800ExtraBold_Italic,
+  NotoSans_900Black,
+  NotoSans_900Black_Italic,
+} from '@expo-google-fonts/noto-sans';
+import {
+  
+  Nunito_200ExtraLight,
+  Nunito_300Light,
+  Nunito_400Regular,
+  Nunito_500Medium,
+  Nunito_600SemiBold,
+  Nunito_700Bold,
+  Nunito_800ExtraBold,
+  Nunito_900Black,
+  Nunito_200ExtraLight_Italic,
+  Nunito_300Light_Italic,
+  Nunito_400Regular_Italic,
+  Nunito_500Medium_Italic,
+  Nunito_600SemiBold_Italic,
+  Nunito_700Bold_Italic,
+  Nunito_800ExtraBold_Italic,
+  Nunito_900Black_Italic,
+} from '@expo-google-fonts/nunito';
+export default function SecondPage({navigation}) {
+  let [fontsLoaded] = useFonts({
+    NotoSans_100Thin,
+    NotoSans_100Thin_Italic,
+    NotoSans_200ExtraLight,
+    NotoSans_200ExtraLight_Italic,
+    NotoSans_300Light,
+    NotoSans_300Light_Italic,
+    NotoSans_400Regular,
+    NotoSans_400Regular_Italic,
+    NotoSans_500Medium,
+    NotoSans_500Medium_Italic,
+    NotoSans_600SemiBold,
+    NotoSans_600SemiBold_Italic,
+    NotoSans_700Bold,
+    NotoSans_700Bold_Italic,
+    NotoSans_800ExtraBold,
+    NotoSans_800ExtraBold_Italic,
+    NotoSans_900Black,
+    NotoSans_900Black_Italic,
+    Nunito_200ExtraLight,
+    Nunito_300Light,
+    Nunito_400Regular,
+    Nunito_500Medium,
+    Nunito_600SemiBold,
+    Nunito_700Bold,
+    Nunito_800ExtraBold,
+    Nunito_900Black,
+    Nunito_200ExtraLight_Italic,
+    Nunito_300Light_Italic,
+    Nunito_400Regular_Italic,
+    Nunito_500Medium_Italic,
+    Nunito_600SemiBold_Italic,
+    Nunito_700Bold_Italic,
+    Nunito_800ExtraBold_Italic,
+    Nunito_900Black_Italic,
+  });
+  function _onPressButton() {
+    Alert.alert('You tapped the button!');
+  }
+
+  let fontSize = 24;
+  let paddingVertical = 6;
+  
+  if (!fontsLoaded) {
+    return <BerikoSVGComponent/>;
+  }
+  return(
+
+      
+      <View style={{flex:1,justifyContent:'center',backgroundColor:'#243236',padding:20}}>
+
+          <View style={{ transform: [{ rotate: '30deg' }] , padding:60 }}>
+              <CoolBerikoSVGComponent width={300} height={300}/>
+          </View>
+
+          <View style={{margin:20}}>
+            
+            <View style={{marginTop:20}}>
+              <Text style={styles.subTexts}>
+              Tips and 
+              Tricks
+              </Text>
+              
+              <Text style={styles.subTexts2}>
+              Can now use Berimojis to respond  messages
+              </Text>
+
+            </View>
+          </View>
+
+          <View style={{margin:20,flex:1,flexDirection:'row',padding:2}}>
+          <View style={{paddingLeft:10,paddingRight:10}}> 
+              <DullBerikoSVGComponent width={30} height={30}/> 
+            </View>
+            <View style={{paddingLeft:10,paddingRight:10}}> 
+              <BerikoSVGComponent width={30} height={30}/> 
+            </View>
+            
+            <View style={{paddingLeft:10,paddingRight:10}}> 
+              <DullBerikoSVGComponent width={30} height={30}/> 
+            </View>
+          </View>
+
+          <View style={{flex:1,flexDirection:'row',justifyContent:'space-between',marginTop:15,marginBottom:15,marginLeft:10,marginLeft:10,padding:20}}>
+              <View >
+                <TouchableHighlight onPress={_onPressButton} underlayColor="white">
+            
+                  <Text style={{color:'white',fontFamily:'Nunito_600SemiBold',fontSize:20}}>Skip</Text>
+          
+                </TouchableHighlight>
+                
+              </View>
+              <View >
+              <TouchableOpacity onPress={_onPressButton}>
+                <View style={styles.button}>
+                  <ArrowSVGComponent width={30} height={30}/>
+                </View>
+            </TouchableOpacity>
+                
+              </View>
+
+
+
+
+          </View>
+
+
+          
+
+          
+        
+      </View>
+     
+    
+)};
+const styles=StyleSheet.create({
+ text:{
+  color:'#08cc7e',
+  fontSize:65,
+  fontFamily: 'Nunito_800ExtraBold',
+  
+ },
+ subTexts:{
+  color:'#FEFEFF',
+  fontSize:45,
+  
+  fontFamily:'NotoSans_800ExtraBold'
+  
+
+ },
+ subTexts2:{
+  color:'#FEFEFF',
+  fontSize:25,
+  
+  fontFamily:'Nunito_600SemiBold'
+  
+
+ },
+ button: {
+  marginBottom: 30,
+ justifyContent: 'center',
+  alignItems: 'center',
+  width: 50,
+  height: 50,
+  alignItems: 'center',
+  borderRadius: 100,
+  backgroundColor: '#08cc7e',
+},
+buttonText: {
+  textAlign: 'center',
+  padding: 20,
+  color: 'white',
+},
+
+});
+
