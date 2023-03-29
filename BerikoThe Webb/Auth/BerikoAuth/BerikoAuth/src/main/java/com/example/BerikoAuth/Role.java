d3f422d8b13f623794022f6569dac8326877dac2
Raghavan M <20pw26@psgtech.ac.in>
2023-03-29 20:08:42 +0530

    authapp

---
+++ BerikoThe Webb/Auth/BerikoAuth/BerikoAuth/src/main/java/com/example/BerikoAuth/Role.java
@@ -0,0 +1,36 @@
+package com.example.BerikoAuth;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="roles")
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Enumerated(EnumType.STRING)
+    @Column(length = 20)
+    private RoleEnum name;
+
+    public Role(Integer id, RoleEnum name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public RoleEnum getName() {
+        return name;
+    }
+
+    public void setName(RoleEnum name) {
+        this.name = name;
+    }
+}
---
+++ BerikoThe Webb/Auth/BerikoAuth/BerikoAuth/src/main/java/com/example/BerikoAuth/RoleEnum.java
@@ -0,0 +1,7 @@
+package com.example.BerikoAuth;
+
+public enum RoleEnum {
+    ROLE_USER,
+    ROLE_MODERATOR,
+    ROLE_ADMIN
+}
